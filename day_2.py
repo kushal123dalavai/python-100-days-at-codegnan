5c8d40d6f66fcf23a8e667224b80015046c21451
kushal123dalavai <kushaldalavai@gmail.com>
2026-01-07 17:03:23 +0530

    Add files via upload
    datatypes,variables and IO functions

---
+++ day_2.py
@@ -0,0 +1,13 @@
+name="ksda"
+age=50
+weight=78.785
+list1=[1,1,1,'hi',2.4,(10,20)]
+tuple1=(10,20,30,'Welcome')
+cards={'A',2,4,5}
+marks={'maths':76, 'telugu':78, 'social':78}
+print("My name is :",name)
+print("My age is:",weight)
+print("list of some:",list1)
+print("tuple of items:",tuple1)
+print("cards:",cards)
+print("marks of a student: ",marks)
