e959e4138adfec13093f9ccf1539837129f8fccd
kushal123dalavai <kushaldalavai@gmail.com>
2026-01-08 16:52:10 +0530

    Add files via upload
    used arithematic operators.

---
+++ day3_arithematic_opera.py
@@ -0,0 +1,11 @@
+# simple calculator
+a=10
+b=20
+addition=a+b
+print("sum of two numbers:",addition)
+print("subtaction of two numbers:",a-b)
+print("multiplication of two numbers:",a*b)
+print("division of two numbers is:",b/a)
+print("floor division of two numbers is:",b//a)
+print("modulo of two numbers is:",b%a)
+print("exponential of two numbers is:",b**a)
